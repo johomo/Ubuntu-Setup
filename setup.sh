a0a9140b2154c0a9a9ce7ba0b29ce730a28f1bc5
joptim <28574850+Joptim@users.noreply.github.com>
2021-02-20 22:18:41 +0100

    hello world

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+command -v ansible > /dev/null 2>&1
+if [$? -ne "0"]; then
+    echo "Ansible not found. Installing..."
+    sudo apt update
+    sudo apt install software-properties-common
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    sudo apt install ansible
+fi 
+
+# Run playbook
+ansible-playbook ubuntu_setup.yml --ask-become-pass
